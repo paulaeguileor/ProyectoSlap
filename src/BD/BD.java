c4c9ce8083d8f61431e39206d683d33ba139b26c
sofimanrique8 <sofi.m@opendeusto.es>
2025-12-01 20:04:39 +0100

    Base de datos con los métodos para insertar artículos

--- src/BD/BD.java
+++ src/BD/BD.java
@@ -31,23 +31,37 @@ public class BD {
 	}
 	
 	public void crearTablas() {
-		String sqlUsuarios = "CREATE TABLE IF NOT EXISTS Usuarios("
-				+"nombre VARCHAR(15) PRIMARY KEY,"
-				+"email VARCHAR(30),"
-				+"telefono INTEGER),"
-				+"direccion VARCHAR(40)";
-		String sqlArticulos = "CREATE TABLE IF NOT EXISTS Articulos("
-				+"id CHAR(5) PRIMARY KEY,"
-				+"nombre VARCHAR(15),"
-				+"numCantidad INTEGER,"
-				+ "talla CHAR(1)";
-		try {
-			Statement st = con.createStatement();
-			st.execute(sqlUsuarios);
-			st.execute(sqlArticulos);
-			st.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+	    String sqlUsuarios = 
+	        "CREATE TABLE IF NOT EXISTS Usuarios (" +
+	        "nombre VARCHAR(15) PRIMARY KEY," +
+	        "email VARCHAR(30)," +
+	        "telefono INTEGER," +
+	        "direccion VARCHAR(40)" +
+	        ")";
+
+	    String sqlArticulos = 
+	        "CREATE TABLE IF NOT EXISTS Articulos (" +
+	        "id CHAR(5) PRIMARY KEY," +
+	        "nombre VARCHAR(15)," +
+	        "numCantidad INTEGER," +
+	        "talla CHAR(1)" +
+	        ")";
+
+	    try {
+	        Statement st = con.createStatement();
+	        st.execute(sqlUsuarios);
+	        st.execute(sqlArticulos);
+	        st.close();
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    }
+	}
+
+	
+	public static void main(String[] args) {
+		BD db = new BD();
+		db.initBD("slap.db");
+		db.crearTablas();
+		db.closeBD();
 	}
 }
